37334f7df0a5dd542315fffa3bd72595b36b4cfd
mahesh183 <maheshaurad.kal@gmail.com>
2019-06-13 10:57:03 +0530

    work thirsday

--- StringPermutations.java
+++ StringPermutations.java
@@ -10,75 +10,61 @@
 import java.util.Scanner;
 
 public class StringPermutations{
-    static int DistinctPermutationCount=0,k;           //
-    static int lenght,newlenght=0;
-    static int count1=0;
-    static int factorial=1,possiblePermuatationlenght;
-   static String[] distinctPermutationStringArray = new String[10000];
+    static int DistinctPermutationCount=0,k;           //count number of distinct permuatations
+    static int lenght,newlenght=0;                     //lenght of the input string,newlenght is lenght of the disticnt permutation array of string
+    static int factorial=1,possiblePermuatationlenght; //factorial of the lenght of the string for dynamic array length and set it to possiblePermuatationlenght 
+   static String[] distinctPermutationStringArray = new String[10000]; //array of string for distinct string permutations
 
     public static void main(String arg[]){
        
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter String");
+        System.out.println("Enter String");  //get input string from user
         String inputString=sc.nextLine();
         lenght=inputString.length();
-        possiblePermuatationlenght = lenght;
+        possiblePermuatationlenght = lenght; //set that lenght to possiblePermuatationlenght
+
+        //get possible lenght of combinataion of input string 
         while(possiblePermuatationlenght != 0){
             factorial = factorial*possiblePermuatationlenght;
             possiblePermuatationlenght--;
         }
-        possiblePermuatationlenght = factorial;
+        possiblePermuatationlenght = factorial;  //after getting the possible length of combination of string set to  possiblePermuatationlenght
        
-        
-
         System.out.println("length of input string is : "+lenght);
-        StringPermutations stringPermutation = new StringPermutations();
+        StringPermutations stringPermutationObj = new StringPermutations();
 
        try{
         System.out.println("Distinct permutations are:");
         System.out.print("[");
-        stringPermutation.permute(inputString,0,lenght-1);
-        System.out.print("]");
+        stringPermutationObj.permute(inputString,0,lenght-1);
+        
+        for(int i=0;i<newlenght;i++){
+            System.out.print(distinctPermutationStringArray[i]+"\t");
+        }
+
+        System.out.println("]");
         System.out.println("number of distinct permuatations are:"+DistinctPermutationCount);
-       }
+       
+    
+        }
+        //if the exception occer then it catch into the catch block
        catch(ArrayIndexOutOfBoundsException e){
         System.out.println("The index you have entered is invalid");
         System.out.println("Please enter an index number between 0 and 6");
        }
-       
-       // stringPermutation.distinctPermutations();
-        
-         sc.close();
+         sc.close(); //close the allocated resource
    
     }
      /** 
      * permutation method 
-     * @param str string to calculate permutation for 
-     * @param l starting index 
-     * @param r end index 
+     * @paramstr string to calculate permutation for 
+     * @param start starting index 
+     * @param end end index 
      */
     private void permute(String paramString,int start,int end){
-        int i=0,icnt=1;
+        int i=0;
         if(start == end){
-            
-            for(i=0;i<newlenght;i++){
-                if(distinctPermutationStringArray[i].equals(paramString)){
-                    icnt++;
-                    break;
-                }
-            }
-            if(newlenght==0){
-                distinctPermutationStringArray[newlenght] = paramString;
-                System.out.print(distinctPermutationStringArray[newlenght]+"\t");
-                newlenght++;
-                DistinctPermutationCount++;
-                icnt++;
-            }else if(icnt==1){
-                distinctPermutationStringArray[newlenght] = paramString;
-                System.out.print(distinctPermutationStringArray[newlenght]+"\t");
-                newlenght++;
-                DistinctPermutationCount++;
-            }
+            getDistinctString(paramString);
         }
         else{
             for( i=start;i<=end;i++){
@@ -90,9 +76,18 @@ public class StringPermutations{
         }
         
     }
-    public String swap(String s,int i,int j){
+
+/** 
+     * Swap Characters at position 
+     * @param string string value 
+     * @param i position 1 
+     * @param j position 2 
+     * @return swapped string 
+*/
+
+    public String swap(String string,int i,int j){
         char temp;
-        char[] charArray = s.toCharArray();
+        char[] charArray = string.toCharArray();
         temp= charArray[i];
         charArray[i]=charArray[j];
         charArray[j]=temp;
@@ -100,79 +95,33 @@ public class StringPermutations{
 
     }
 
-    // public void distinctPermutations(){
-    //     for(int i=0;i<count-1;i++){
-    //         k=0;
-    //         for(int j=0;j<count;j++){
-    //              if(permutationsStringArray[i].equals(permutationsStringArray[j])){
-    //                 break;         
-    //             }
-    //             k++;
-    //         }
-    //         if(k==i)
-    //         {
-    //             distinctPermutationStringArray[count1]=permutationsStringArray[i];
-    //                count1++;
-    //         }
-    //     }
-
-    //         System.out.print("[");
-    //     for(int i=0;i<count1;i++){
-            
-    //         System.out.print(distinctPermutationStringArray[i]+"\t");
-    //     }
-    //     System.out.print("]");
-    //      System.out.println(count);
-
-    // }
+/**
+    * get distinct string from permutaions  
+     * @param paramString string value 
+*/
+    
+    public static void getDistinctString(String paramString){
+        int i=0,icnt=1;
+        for(i=0;i<newlenght;i++){   
+            if(distinctPermutationStringArray[i].equals(paramString)){
+                icnt++;
+                break;
+            }
+        }
+        if(newlenght==0){
+            distinctPermutationStringArray[newlenght] = paramString;
+            //System.out.print(distinctPermutationStringArray[newlenght]+"\t");
+            newlenght++;
+            DistinctPermutationCount++;
+            icnt++;
+        }else if(icnt==1){
+            distinctPermutationStringArray[newlenght] = paramString;
+            //System.out.print(distinctPermutationStringArray[newlenght]+"\t");
+            newlenght++;
+            DistinctPermutationCount++;
+        }
+    }
 }
-// import java.util.Scanner;
-// // Java program to print all permutations of a 
-// // given string. 
-// public class StringPermutations { 
-//     public static void main(String[] args) 
-//     { 
-//         String str = "ABC"; 
-//         int n = str.length(); 
-//         StringPermutations permutation = new StringPermutations(); 
-//         permutation.permute(str, 0, n - 1); 
-//     } 
-  
-    /** 
-     * permutation function 
-     * @param str string to calculate permutation for 
-     * @param l starting index 
-     * @param r end index 
-     */
-//     private void permute(String str, int l, int r) 
-//     { 
-//         if (l == r) 
-//             System.out.println(str); 
-//         else { 
-//             for (int i = l; i <= r; i++) { 
-//                 str = swap(str, l, i); 
-//                 permute(str, l + 1, r); 
-//                 str = swap(str, l, i); 
-//             } 
-//         } 
-//     } 
-  
-//     /** 
-//      * Swap Characters at position 
-//      * @param a string value 
-//      * @param i position 1 
-//      * @param j position 2 
-//      * @return swapped string 
-//      */
-//     public String swap(String a, int i, int j) 
-//     { 
-//         char temp; 
-//         char[] charArray = a.toCharArray(); 
-//         temp = charArray[i]; 
-//         charArray[i] = charArray[j]; 
-//         charArray[j] = temp; 
-//         return String.valueOf(charArray); 
-//     } 
-// } 
-  
-// // This code is contributed by Mihir Joshi 
+
+// End of the programme
+    
