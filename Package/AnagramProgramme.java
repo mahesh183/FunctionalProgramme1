47ddc0df60dd0a527da48c6df58dfe300fcf1626
mahesh183 <maheshaurad.kal@gmail.com>
2019-06-17 16:21:54 +0530

    algorithm Programme

--- Package/AnagramProgramme.java
+++ Package/AnagramProgramme.java
@@ -1,9 +1,17 @@
 import MyPackage.Util;
 import java.util.Scanner;
+import java. sql. Timestamp;
+import java.util.Date;
+
 class AnagramProgramme{
     public static void main(String args[]){
         Scanner sObj = new Scanner(System.in);
         boolean checkAnagram=false;
+        Date date=new Date();
+        long time = date. getTime();
+        System. out. println("Time in Milliseconds : "+time);
+        Timestamp ts = new Timestamp(time); 
+        System. out. println("Current Time Stamp 1: "+ts);
         System.out.println("Enter the First String:");
         String firstString=sObj.nextLine();
 
@@ -11,7 +19,9 @@ class AnagramProgramme{
         String secondString=sObj.nextLine();
 
         checkAnagram=Util.Anagram(firstString,secondString);
-        
+        long time1 = date. getTime();
+        System. out. println("Current Time Stamp 2: "+ts);
+        System. out. println("Time in Milliseconds 1: "+time1);
         if(checkAnagram==true)
         {
             System.out.println("The given string are anagram");
